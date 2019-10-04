e0c68b5a76dc729415d610574c459a1abec4c5a6
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2019-10-04 15:24:56 +0100

    Fixed file list

--- clusters_retriever.py
+++ clusters_retriever.py
@@ -511,7 +511,7 @@ def particle_metallicity(path, file, part_type = '0'):
 	return part_metallicity
 
 
-print(file_dir_hdf5(subject= 'groups', redshift = 0.0)[0:2])
+print(file_dir_hdf5(subject= 'particledata', redshift = 0.0))
 
 
 
