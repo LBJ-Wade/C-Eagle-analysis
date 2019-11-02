c9e6427c502d64289626f49138ebabc40b109dbf
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2019-11-02 19:02:00 +0000

    Stable (hopefully) version.

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from cluster_profiler import *
 
 ceagle = Simulation()
 z_catalogue = ceagle.get_redshiftAllowed(dtype = float)
-cluster = Cluster(clusterID = 0, redshift = 0.101)
+cluster = Cluster(clusterID = 0, redshift = z_catalogue[-1])
 print(cluster.group_centre_of_potential())
