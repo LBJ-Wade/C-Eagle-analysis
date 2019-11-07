91aad55c7bf9834142f4768534fafc8491a8fa21
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2019-11-07 00:12:38 +0000

    Test particledata

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
     print(cluster.group_centre_of_potential())
     print(cluster.NumOfSubhalos(central_FOF = True))
     # print(cluster.subgroups_number(central_FOF = True))
-    print(cluster.particle_coordinates('0'))
+    print(cluster.group_number_part('0'))
+    print(cluster.subgroup_number_part('0'))
 
 
