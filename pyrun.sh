1e1566b9dec663952cf8c7933dfd93e261ab3f85
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2019-11-10 17:56:21 +0000

    Test thermal idx

--- pyrun.sh
+++ pyrun.sh
@@ -1,8 +1,8 @@
 module unload python/2.7.15
 module load python/3.6.5
 python3 --version
-echo -e "\e[1m \e[92m Syncing GitHub..."
+echo -e "\e[91mSyncing GitHub...\e[0m"
 git pull
-echo Running main program...
+echo -e "\e[1m\e[91mRunning main program...\e[0m\e[92m"
 python3 main.py
-echo End of session.
+echo -e "\e[0m\e[5m\e[1m\e[91mEnd of session.\e[0m"
