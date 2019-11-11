d563fede80266e41b8ed52b573ec3c3adfc61524
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2019-11-11 23:46:24 +0000

    Test thermal idx

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
-__FILE__ = """
-main.py
-
+__FILE__ = """------------------------------------------------------------------
+FILE:   main.py
 AUTHOR: Edo Altamura
-DATE: 10-11-2019
-
+DATE:   10-11-2019
+------------------------------------------------------------------
 This file contains the main() function and is used for testing
 purposes. It can also be linked to shell arguments for profiling.
 By setting __PROFILE__ = False you are choosing to run the
 program with normal outputs, while __PROFILE__ = True will trigger
 the profiler and display the call stats associated with main().
-"""
+-------------------------------------------------------------------"""
 
 __PROFILE__ = False
 
+
+def time_func(function):
+    # create a new function based on the existing one,
+    # that includes the new timing behaviour
+    def new_func(*args, **kwargs):
+        start = datetime.datetime.now()
+        print('Start: {start}')
+
+        function_result = function(*args, **kwargs)
+        # Calculate the elapsed time and add it to the function
+        # attributes.
+        end = datetime.datetime.now()
+        new_func.elapsed = end - start
+        print('End: {end}')
+        print('Elapsed: {new_func.elapsed}')
+        return function_result
+    return new_func
+
+@time_func
 def main():
 
     from clusters_retriever import Cluster
@@ -42,13 +60,9 @@ if __name__ == "__main__":
 
     if __PROFILE__:
         import cProfile
-
-        datetime.datetime.now().isoformat()
         cProfile.run('main()')
-        datetime.datetime.now().isoformat()
+
     else:
         print(__FILE__)
-        datetime.datetime.now().isoformat()
         main()
-        datetime.datetime.now().isoformat()
 
