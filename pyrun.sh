e04db9a1ad103d464875c91ed383e69d9324c5b0
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2019-11-13 20:36:56 +0000

    Performance display branched.

--- pyrun.sh
+++ pyrun.sh
@@ -1,9 +1,9 @@
 module unload python/2.7.15
 module load python/3.6.5
 python3 --version
-echo -e "\e[91mSyncing GitHub...\e[0m"
+echo -e "\e[91mSyncing GitHub...\e[0m\e[93"
 git pull
-echo -e "\e[1m\e[91mRunning main program...\e[0m\e[92m"
+echo -e "\e[0m\e[1m\e[91mRunning main program...\e[0m\e[92m"
 #python3 -u ./main.py > ./main.log &
 python3 ./main.py
 echo -e "\e[0m\e[5m\e[1m\e[91mEnd of session.\e[0m"
