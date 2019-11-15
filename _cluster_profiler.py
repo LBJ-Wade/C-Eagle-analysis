0c5b52fb6057c228a509f6be8a5ae8efb72ece69
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2019-11-15 15:29:53 +0000

    Mergers test.

--- _cluster_profiler.py
+++ _cluster_profiler.py
@@ -80,7 +80,7 @@ class Mixin:
             group_CoP = self.group_centre_of_potential()
             coords = np.subtract(coords, group_CoP)
             r = np.linalg.norm(coords, axis=1)
-            index = np.where((r < r500) & (group_num == 0))[0]
+            index = np.where((r < r500) & (group_num == 1))[0]
             mass = mass[index]
             coords = coords[index]
             assert mass.__len__() > 0, "Array is empty - check filtering."
@@ -125,7 +125,7 @@ class Mixin:
             group_CoP = self.group_centre_of_potential()
             coords = np.subtract(coords, group_CoP)
             r = np.linalg.norm(coords, axis=1)
-            index = np.where((r < r500) & (group_num == 0))[0]
+            index = np.where((r < r500) & (group_num == 1))[0]
             mass = mass[index]
             vel = vel[index]
             assert mass.__len__() > 0, "Array is empty - check filtering.."
