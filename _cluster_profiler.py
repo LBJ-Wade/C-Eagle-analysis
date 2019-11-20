eb0bd6611829a857338c3368696d41a4ceb95f5a
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2019-11-20 14:13:29 +0000

    Centre of mass testing.

--- _cluster_profiler.py
+++ _cluster_profiler.py
@@ -34,8 +34,9 @@ class Mixin:
         te = 1.5 * k_B * temperature * mass * 0.88 / (1.6735575* np.power(10, -27.))
         return np.sum(te)
 
-    @numba.guvectorize([numba.int64(numba.int64[:], numba.int64[:]),
-                        numba.int32(numba.int32[:], numba.int32[:])],
+    @numba.guvectorize(['numba.int64(numba.int64[:], numba.int64[:])',
+                        'numba.int32(numba.int32[:], numba.int32[:])',
+                        ],
                        '(n),(n)->()', nopython=True, target='parallel')
     @staticmethod
     def angular_momentum(mass, velocity, position):
