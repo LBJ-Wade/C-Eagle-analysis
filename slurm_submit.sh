ef2f51865f9434d579c943fea0b051878865aaf8
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-01-29 13:24:10 +0000

    Specify SLURM submission scripts - submitted cosma6.

--- slurm_submit.sh
+++ slurm_submit.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -l
 
-#SBATCH --ntasks 144                                           # The number of cores you need...
-#SBATCH -J CELR_stage1                                         # Give it something meaningful.
+#SBATCH --ntasks 143                                           # The number of cores you need...
+#SBATCH -J CELR_red                                            # Give it something meaningful.
 #SBATCH -o standard_output_file.%J.out
 #SBATCH -e standard_error_file.%J.err
 #SBATCH -p cosma6                                              # or some other partition, e.g. cosma, cosma6, etc.
 #SBATCH -A dp004                                               # e.g. dp004
 #SBATCH --exclusive
 #SBATCH -t 72:00:00
-#SBATCH --mail-type=END                            # notifications for job done & fail
+#SBATCH --mail-type=END                                        # notifications for job done & fail
 #SBATCH --mail-user=<edoardo.altamura@manchester.ac.uk>        #PLEASE PUT YOUR EMAIL ADDRESS HERE (without the <>)
 
 module purge
@@ -18,8 +18,6 @@ module load python/3.6.5
 
 module load intel_comp/2019
 module load openmpi
-module load hdf5
-
 
 # Run the program
-mpiexec -n $SLURM_NTASKS python3 /cosma/home/dp004/dc-alta2/C-Eagle-analysis/main.py
+mpiexec -n $SLURM_NTASKS python3 ./main.py
