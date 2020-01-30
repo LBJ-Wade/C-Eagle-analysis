15217fc3e111d0b6897429182b98a7afb97bdf68
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-01-30 16:49:18 +0000

    Run batch with parallelised output.

--- slurm_submit.sh
+++ slurm_submit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 
-#SBATCH --ntasks 350                                           # The number of cores you need...
+#SBATCH --ntasks 352                                           # The number of cores you need...
 #SBATCH -J CELR_red                                            # Give it something meaningful.
 #SBATCH -o standard_output_file.%J.out
 #SBATCH -e standard_error_file.%J.err
@@ -22,3 +22,8 @@ module load hdf5
 
 # Run the program
 mpiexec -n $SLURM_NTASKS python3 ./main.py
+
+
+
+# Multiples of 32, 64, 96, 128, 160, 192, 224, 256, 288, 320, 352, 384, 416, 448, 480, 512, 544, 576, 608
+# Multiples of 28, 56, 84, 112, 140, 168, 196, 224, 252, 280, 308, 336, 364, 392, 420, 448, 476, 504, 532
