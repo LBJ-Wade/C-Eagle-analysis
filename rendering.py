4cc0188efc2e3b060da4c741f77aef1816c84f30
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-02-10 17:17:40 +0000

    Derotation implemented and stable.

--- rendering.py
+++ rendering.py
@@ -272,11 +272,11 @@ def plot_angularmomentum_vectors(vectors,
         print('[ PLOT 3D VECTOR ]\t==>\tDrawing observer_LineOfSight.')
 
         # Draw reference rotation vector
-        Reference_Ang_Momentum_color = '#E59813'
-        Reference_Ang_Momentum = Arrow3D([0, 0], [0, 0], [0, 1], mutation_scale=20, lw=3, arrowstyle="-|>", color=Reference_Ang_Momentum_color)
-        axes.scatter([], [], c=Reference_Ang_Momentum_color, marker=r"$\longrightarrow$", s=70, label=r'Reference angular momentum')
-        axes.add_artist(Reference_Ang_Momentum)
-        print('[ PLOT 3D VECTOR ]\t==>\tDrawing Reference_Ang_Momentum.')
+        # Reference_Ang_Momentum_color = '#E59813'
+        # Reference_Ang_Momentum = Arrow3D([0, 0], [0, 0], [0, 1], mutation_scale=20, lw=3, arrowstyle="-|>", color=Reference_Ang_Momentum_color)
+        # axes.scatter([], [], c=Reference_Ang_Momentum_color, marker=r"$\longrightarrow$", s=70, label=r'Reference angular momentum')
+        # axes.add_artist(Reference_Ang_Momentum)
+        # print('[ PLOT 3D VECTOR ]\t==>\tDrawing Reference_Ang_Momentum.')
 
 
 
@@ -351,13 +351,14 @@ def derotate_field():
     rotated_vector = cluster.apply_rotation_matrix(rotation, vector_to_rotate); print(rotated_vector)
 
 
-    legend_labels = [r'$\mathrm{Gas}$',
-                     r'$\mathrm{Highres DM}$',
-                     r'$\mathrm{Stars}$',
-                     r'$\mathrm{Black holes}$']
+    legend_labels = [r'vector_reference',
+                     r'vector_to_rotate_1',
+                     r'vector_to_rotate_2',
+                     r'vector_to_rotate_3',
+                     r'rotated_vector']
 
     plot_angularmomentum_vectors(np.vstack((vector_reference, vector_to_rotate, rotated_vector)),
-                                 labels = None,
+                                 labels = legend_labels,
                                  axes=None,
                                  plot_unitSphere=True,
                                  normalise_length=False,
