b2a4d957fc32f676abb001437cbe9b390b470196
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-02-17 15:32:21 +0000

    Implemented MACSIS support.

--- visualisation/yt.py
+++ visualisation/yt.py
@@ -4,24 +4,13 @@ import numpy as np
 import yt
 ds = yt.load('/cosma5/data/dp004/C-EAGLE/Complete_Sample/CE_00/data/particledata_029_z000p000/eagle_subfind_particles_029_z000p000.0.hdf5')
 
-sc = yt.create_scene(ds, lens_type='perspective')
 
-source = sc[0]
+# This is an object that describes the entire box
+ad = ds.all_data()
 
-source.set_field('density')
-source.set_log(True)
+# We plot the average velocity magnitude (mass-weighted) in our object
+# as a function of density and temperature
+plot = yt.PhasePlot(ad, "density", "temperature", "velocity_magnitude")
 
-bounds = (3e-31, 5e-27)
-
-# Since this rendering is done in log space, the transfer function needs
-# to be specified in log space.
-tf = yt.ColorTransferFunction(np.log10(bounds))
-
-tf.add_layers(5, colormap='arbre')
-
-source.tfh.tf = tf
-source.tfh.bounds = bounds
-
-source.tfh.plot('transfer_function.png', profile_field='density')
-
-sc.save('~/rendering.png', sigma_clip=6)
+# save the plot
+plot.save('~/rendering.png')
