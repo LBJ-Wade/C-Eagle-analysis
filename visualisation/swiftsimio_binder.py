906f2db974f4dbf84597b7989943785e2252210c
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-02-17 20:07:01 +0000

    SPH kernel from swiftsimio implemented.

--- visualisation/swiftsimio_binder.py
+++ visualisation/swiftsimio_binder.py
@@ -145,25 +145,22 @@ if __name__ == '__main__':
 
 
 
-    temp_map = generate_map(x, y, m, h, res, parallel=True)
+    temp_map = generate_map(x, y, np.log10(mass), h, res, parallel=True)
     norm = colors.SymLogNorm(linthresh=np.percentile(np.abs(m), 10), linscale=0.5, vmin=-np.abs(m).max(), vmax=np.abs(
         m).max())
 
     from matplotlib import pyplot as plt
 
-    fig = plt.figure(figsize=(6, 3.2))
+    fig = plt.figure(figsize=(6, 6))
 
     ax = fig.add_subplot(111)
     ax.set_title('colorMap')
-    plt.imshow(temp_map, norm= norm, extent=[np.min(coords[:,0]),np.max(coords[:,0]),
+    plt.imshow(temp_map, extent=[np.min(coords[:,0]),np.max(coords[:,0]),
                                              np.min(coords[:,1]),np.max(coords[:,1])])
-    ax.set_aspect('equal')
 
     cax = fig.add_axes([0.12, 0.1, 0.78, 0.8])
     cax.get_xaxis().set_visible(False)
     cax.get_yaxis().set_visible(False)
-    cax.patch.set_alpha(0)
-    cax.set_frame_on(False)
     plt.colorbar(orientation='vertical')
     plt.show()
 
