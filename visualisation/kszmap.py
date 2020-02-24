f063eecc68a93ee847e91dd147e09304dd7746f2
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-02-24 19:48:27 +0000

    Implement progressbar

--- visualisation/kszmap.py
+++ visualisation/kszmap.py
@@ -91,7 +91,7 @@ class KSZMAP:
 
 if __name__ == '__main__':
     # Create a cluster object
-    cluster = Cluster(simulation_name='celr_e', clusterID=0, redshift='z000p000')
+    cluster = Cluster(simulation_name='ceagle', clusterID=0, redshift='z000p000')
     # Create a KSZMAP object and link it to the cluster object
     test_map = KSZMAP(cluster)
     # Test the map output
