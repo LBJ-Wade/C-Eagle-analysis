53d91a84b87fbc7b02c2c1b514647eb19d8bf570
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-02-25 17:10:12 +0000

    Verbose update

--- memory.py
+++ memory.py
@@ -231,9 +231,28 @@ if __name__ == '__main__':
     test = TEST()
     # test.from_dictionary()
     # test.from_cluster()
-    import numpy as np
-    data = np.linspace(0, 100, 101)
-    test.scatter_data(data)
+    # import numpy as np
+    # data = np.linspace(0, 100, 101)
+    # test.scatter_data(data)
+
+    import numpy
+    from mpi4py import MPI
+
+    comm = MPI.COMM_WORLD;
+    rank = comm.Get_rank()
+    if rank == 0:
+        x = numpy.linspace(0, 100, 11)
+        print(x)
+    else:
+        x = None
+    if rank == 2:
+        xlocal = numpy.zeros(9)
+    else:
+        xlocal = numpy.zeros(1)
+    comm.Scatterv([x, (1, 1, 9), (0, 1, 2), MPI.DOUBLE], xlocal)
+    print("process " + str(rank) + " has " + str(xlocal))
+    if (rank == 0):
+        print(x)
 
 
 
