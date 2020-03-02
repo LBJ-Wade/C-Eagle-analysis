34d5ab5abde769eebb257ca46e0abcd6d6194204
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-03-02 19:17:59 +0000

    Light/Dark mode update

--- visualisation/rendering.py
+++ visualisation/rendering.py
@@ -523,6 +523,7 @@ class TestSuite(Map, LosGeometry):
 
         diagram = LosGeometry(fig, axes)
         diagram.set_inset_geometry(0.6, 0.0, 0.4, 0.4)
+        diagram.set_observer(-np.pi/2, 0)
         vectors = [
             [0,1,1],
             [2,5,6],
