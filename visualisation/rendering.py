2d10fbd2df0705a703077cf03b1f2a615056dda6
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-03-02 20:04:40 +0000

    Derotate observer

--- visualisation/rendering.py
+++ visualisation/rendering.py
@@ -539,7 +539,7 @@ class TestSuite(Map, LosGeometry):
 
         diagram = LosGeometry(fig, axes)
         diagram.set_inset_geometry(0.6, 0.0, 0.4, 0.4)
-        diagram.set_observer(rot_x=0, rot_y=-90, rot_z=0)
+        diagram.set_observer(rot_x=-90, rot_y=0, rot_z=0)
         vectors = [
             [0,1,1],
             [2,5,6],
