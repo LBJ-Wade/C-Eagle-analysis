4e852c21e3374137c88829d78a5e61579cefe7c2
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-03-10 22:54:35 +0000

    Aperture push

--- save/fof_output.py
+++ save/fof_output.py
@@ -258,12 +258,12 @@ class FOFDatagen(FOFOutput):
         for r in self.cluster.generate_apertures():
 
             part_mass_aperture = self.cluster.group_mass(out_allPartTypes=True, aperture_radius=r)
-            part_mass = np.append(part_mass, [part_mass_aperture], axis=0)
+            part_mass = np.concatenate((part_mass, [[part_mass_aperture]]), axis=0)
             print(part_mass)
 
             tot_mass_aperture = np.sum(part_mass_aperture)
             print(tot_mass_aperture)
-            total_mass = np.append(total_mass, [tot_mass_aperture], axis=0)
+            total_mass = np.concatenate((total_mass, [[tot_mass_aperture]]), axis=0)
 
         data = {'/Total_mass': np.array(total_mass),
                 '/ParType0_mass' : np.array(part_mass)[:,0],
