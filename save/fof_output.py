e0c3144c26f133f4a1ada5eb9cb14317a2741bbc
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-03-11 19:09:49 +0000

    Fix CoP reshape problem

--- save/fof_output.py
+++ save/fof_output.py
@@ -402,6 +402,54 @@ class FOFDatagen(save.SimulationOutput):
         out = FOFOutput(self.cluster, filename='peculiar_velocity.hdf5', data=data, attrs=attributes)
         out.makefile()
 
+    # TODO
+    def push_kinetic_energy(self):
+        
+        part_kin_energy = np.zeros((0, 4), dtype=np.float)
+        total_kin_energy = np.zeros((0,), dtype=np.float)
+
+        for r in self.cluster.generate_apertures():
+            part_kin_energy_aperture = self.cluster.kinetic_energy(out_allPartTypes=True, aperture_radius=r)
+            part_kin_energy = np.concatenate((part_kin_energy, [part_kin_energy_aperture]), axis=0)
+
+            tot_kin_energy_aperture = np.sum(part_kin_energy_aperture)
+            total_kin_energy = np.concatenate((total_kin_energy, [tot_kin_energy_aperture]), axis=0)
+
+        data = {'/Total_kin_energy': np.array(total_kin_energy),
+                '/ParType0_kin_energy': np.array(part_kin_energy)[:, 0],
+                '/ParType1_kin_energy': np.array(part_kin_energy)[:, 1],
+                '/ParType4_kin_energy': np.array(part_kin_energy)[:, 2],
+                '/ParType5_kin_energy': np.array(part_kin_energy)[:, 3]}
+
+        attributes = {'Description': """The ParType_mass array contains the mass enclosed within a given aperture, 
+                for each particle type (in the order 0, 1, 4, 5).
+                The Total-mass array gives the total mass within an aperture of all partTypes summed together.""",
+                      'Units': '10^10 M_sun'}
+
+        out = FOFOutput(self.cluster, filename='kinetic_energy.hdf5', data=data, attrs=attributes)
+        out.makefile()
+
+    # TODO
+    def push_thermal_energy(self):
+        
+        part_th_energy = np.zeros((0,), dtype=np.float)
+
+        for r in self.cluster.generate_apertures():
+            part_th_energy_aperture = self.cluster.thermal_energy(out_allPartTypes=True, aperture_radius=r)
+            part_th_energy = np.concatenate((part_th_energy, [part_th_energy_aperture]), axis=0)
+
+
+        data = {'/Total_th_energy': np.array(part_th_energy),
+                '/ParType0_th_energy': np.array(part_th_energy)}
+
+        attributes = {'Description': """The ParType_mass array contains the mass enclosed within a given aperture, 
+                for each particle type (in the order 0, 1, 4, 5).
+                The Total-mass array gives the total mass within an aperture of all partTypes summed together.""",
+                      'Units': '10^10 M_sun'}
+
+        out = FOFOutput(self.cluster, filename='thermal_energy.hdf5', data=data, attrs=attributes)
+        out.makefile()
+
 
     def push_angular_momentum(self):
 
@@ -504,7 +552,52 @@ class FOFDatagen(save.SimulationOutput):
         out.makefile()
 
     def push_thermodynamic_merging_index(self):
-        pass
+        """
+        Computes the dynamical index based on the centre of potential coordinates, the centre of mass coordinates
+        and the aperture radius.
+
+        dynamical_merging_index = || CoM(r) - CoP(r) || / r
+
+        The calculation of the dynamical_merging_index assumes the CoM and aperture data are already generated as
+        partial results.
+
+        :return: None
+        """
+        assert os.path.isfile(os.path.join(self.FOFDirectory, 'kinetic_energy.hdf5')), ("Kinetic energy data not "
+                                                                                        f"found in {self.FOFDirectory}."
+                                                                                        "Check that they have "
+                                                                                        "already been computed for "
+                                                                                        "this cluster and this "
+                                                                                        "redshift")
+
+        assert os.path.isfile(os.path.join(self.FOFDirectory, 'thermal_energy.hdf5')), ("Thermal energy data not "
+                                                                                   f"found in {self.FOFDirectory}."
+                                                                                   "Check that they have already been "
+                                                                                   "computed for this cluster and this redshift.")
+
+        # Read kinetic_energy data
+        with h5py.File(os.path.join(self.FOFDirectory, 'kinetic_energy.hdf5'), 'r') as input_file:
+            Total_kin_energy = np.array(input_file.get('Total_kin_energy'))
+
+        # Read thermal_energy data
+        with h5py.File(os.path.join(self.FOFDirectory, 'thermal_energy.hdf5'), 'r') as input_file:
+            Total_th_energy = np.array(input_file.get('Total_th_energy'))
+
+        thermindex = Total_kin_energy/Total_th_energy
+
+        data = {'/Total_thermindex': np.array(thermindex),
+                '/ParType0_thermindex': np.array(thermindex)}
+
+        attributes = {'Description': """Datasets with the thermodynamic merging index of the cluster, calculated 
+                from particles within a specific aperture radius from the Centre of Potential. As the thermal energy 
+                is only defined for gas particles, only ParType0 exists and it is the same as Total_thermindex.
+                The thermodynamic merging index is calculated for a given aperture radius as:
+                thermindex = Total_kin_energy/Total_th_energy.
+                """,
+                      'Units': '[None]'}
+
+        out = FOFOutput(self.cluster, filename='thermal_energy.hdf5', data=data, attrs=attributes)
+        out.makefile()
 
     def push_substructure_merging_index(self):
         pass
@@ -513,118 +606,46 @@ class FOFDatagen(save.SimulationOutput):
 
 if __name__ == '__main__':
 
-    cluster = Cluster(simulation_name = 'celr_e', clusterID = 0, redshift = 'z000p000')
-    out = FOFDatagen(cluster)
-    # out.push_R_crit()
-    # out.push_apertures()
-    # out.push_mass()
-    # out.push_centre_of_mass()
-    # out.push_peculiar_velocity()
-    # out.push_angular_momentum()
-    out.push_dynamical_merging_index()
+    def test():
+        cluster = Cluster(simulation_name = 'celr_e', clusterID = 0, redshift = 'z000p000')
+        out = FOFDatagen(cluster)
+        # out.push_R_crit()
+        # out.push_apertures()
+        # out.push_mass()
+        # out.push_centre_of_mass()
+        # out.push_peculiar_velocity()
+        # out.push_angular_momentum()
+        # out.push_dynamical_merging_index()
+        out.push_kinetic_energy()
+        out.push_thermal_energy()
+        out.push_thermodynamic_merging_index()
+        out.push_substructure_merging_index()
 
+    def mining():
+        """
+        RECORD
+        -----------------------
+        celr_e:
+            out.push_R_crit()
+            out.push_apertures()
+            out.push_mass()
+            out.push_centre_of_mass()
+            out.push_peculiar_velocity()
+            out.push_angular_momentum()
+        """
+        sim = Simulation(simulation_name='celr_e')
+        iterator = itertools.product(sim.clusterIDAllowed, sim.redshiftAllowed)
+        for halo_id, halo_z in iterator:
+            cluster = Cluster(simulation_name=sim.simulation_name, clusterID=halo_id, redshift=halo_z)
+            out = FOFDatagen(cluster)
+            out.push_R_crit()
+            out.push_apertures()
+            out.push_mass()
+            out.push_centre_of_mass()
+            out.push_peculiar_velocity()
+            out.push_angular_momentum()
 
-
-
-i = 0
-# @progressbar.ProgressBar()
-@make_parallel_MPI
-def MPI_decorator_test(**kwargs):
-    import time
-    nb_iter = 200
-    time.sleep(0.0001)
-    i=+1
-    # yield ((i) / nb_iter)  # Give control back to decorator
-
-
-
-@make_parallel_MPI
-def push_FOFangmom_alignment_matrix(*args, **kwargs):
-    """
-    Saves the angular momentum alignment matrix data into the catalogues.
-    :param simulation: (cluster.Simulation) object
-    :return: None
-    """
-    cluster_obj = kwargs['cluster']
-
-    print('[ FOF SAVE ]\t==>\t AngMom align matrix on cluster {} @ z = {}'.format(cluster_obj.clusterID, cluster_obj.redshift))
-
-    align_matrix = np.zeros((0, 6), dtype=np.float)
-
-    # Loop over apertures
-    for r in cluster_obj.generate_apertures():
-        m = angular_momentum.angular_momentum_PartType_alignment_matrix(cluster_obj, specific_angular_momentum=False, aperture_radius=r)
-
-        # Contract alignment matrix into 1D vector
-        align_matrix_aperture = np.array([m[1][0], m[2][0], m[2][1], m[3][0], m[3][1], m[3][2]])
-        align_matrix = np.concatenate((align_matrix, [align_matrix_aperture]), axis=0)
-
-    assert align_matrix.__len__() == cluster_obj.generate_apertures().__len__()
-
-
-    save.create_dataset(kwargs['fileCompletePath'],
-                        subfolder=__HDF5_SUBFOLDER__,
-                        dataset_name='Group_Angular_Momentum_Alignment_Matrix',
-                        input_data=align_matrix,
-                        attributes="""The alignment matrix elements are calculated for each aperture listed in 
-                        the 
-                        `Aperture dataset`. PartTypes included: 0, 1, 4, 5.
-
-                        Units: degrees
-                        
-                        Element reference:
-                        0 = DM to gas
-                        1 = Stars to gas
-                        2 = Stars to DM
-                        3 = BH to gas
-                        4 = BH to DM
-                        5 = BH to stars
-                        """)
-
-@make_parallel_MPI
-def push_FOFmerging_indices(*args, **kwargs):
-    """
-    Saves the angular momentum alignment matrix data into the catalogues.
-    :param simulation: (cluster.Simulation) object
-    :return: None
-    """
-    cluster_obj = kwargs['cluster']
-
-    print('[ FOF SAVE ]\t==>\t Merging indices on cluster {} @ z = {}'.format(cluster_obj.clusterID, cluster_obj.redshift))
-
-    dynamical_idx = np.zeros(0, dtype=np.float)
-    thermal_idx = np.zeros(0, dtype=np.float)
-
-    # Loop over apertures
-    for r in cluster_obj.generate_apertures():
-        dyn_aperture = mergers.dynamical_index(cluster_obj, aperture_radius=r)
-        therm_aperture = mergers.thermal_index(cluster_obj, aperture_radius=r)
-
-        dynamical_idx = np.concatenate((dynamical_idx, [dyn_aperture]), axis=0)
-        thermal_idx = np.concatenate((thermal_idx, [therm_aperture]), axis=0)
-
-    assert dynamical_idx.__len__() == cluster_obj.generate_apertures().__len__()
-    assert thermal_idx.__len__() == cluster_obj.generate_apertures().__len__()
-
-    save.create_dataset(kwargs['fileCompletePath'],
-                        subfolder=__HDF5_SUBFOLDER__,
-                        dataset_name='Dynamical_Merging_Index',
-                        input_data=dynamical_idx,
-                        attributes="""The dynamical merging indices calculated for each aperture listed in 
-                        the `Aperture dataset`. PartTypes included: 0, 1, 4, 5.
-
-                        Units: Dimensionless
-                        """)
-
-    save.create_dataset(kwargs['fileCompletePath'],
-                        subfolder=__HDF5_SUBFOLDER__,
-                        dataset_name='Thermal_Merging_Index',
-                        input_data=thermal_idx,
-                        attributes="""The thermal merging indices calculated for each aperture listed in 
-                                    the `Aperture dataset`. PartTypes included: 0, 1, 4, 5.
-
-                                    Units: Dimensionless
-                                    """)
+    mining()
 
 
 
