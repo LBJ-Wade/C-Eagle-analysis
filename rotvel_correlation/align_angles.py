c9101f1c3ef0e8b9c147a18ce4a6069407a86f54
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-03-15 19:10:27 +0000

    matrix test

--- rotvel_correlation/align_angles.py
+++ rotvel_correlation/align_angles.py
@@ -19,6 +19,7 @@ import os.path
 import h5py
 import matplotlib
 import matplotlib.pyplot as plt
+from matplotlib import colors
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 
@@ -37,128 +38,6 @@ class CorrelationMatrix(pull.FOFRead):
     def fix_aperture(self, aperture: np.int):
         self.aperture_index = aperture
 
-    @staticmethod
-    def heatmap(data, row_labels, col_labels, ax=None,
-                cbar_kw={}, cbarlabel="", **kwargs):
-        """
-        Create a heatmap from a numpy array and two lists of labels.
-
-        Parameters
-        ----------
-        data
-            A 2D numpy array of shape (N, M).
-        row_labels
-            A list or array of length N with the labels for the rows.
-        col_labels
-            A list or array of length M with the labels for the columns.
-        ax
-            A `matplotlib.axes.Axes` instance to which the heatmap is plotted.  If
-            not provided, use current axes or create a new one.  Optional.
-        cbar_kw
-            A dictionary with arguments to `matplotlib.Figure.colorbar`.  Optional.
-        cbarlabel
-            The label for the colorbar.  Optional.
-        **kwargs
-            All other arguments are forwarded to `imshow`.
-        """
-
-        if not ax:
-            ax = plt.gca()
-
-        # Plot the heatmap
-        im = ax.imshow(data, **kwargs)
-
-        # Create colorbar
-        divider = make_axes_locatable(ax)
-        cax = divider.append_axes("right", size="5%", pad=0.05)
-        cbar = cax.figure.colorbar(im, ax=cax, **cbar_kw)
-        cax.set_ylabel(cbarlabel, rotation=-90, va="bottom")
-
-        # We want to show all ticks...
-        ax.set_xticks(np.arange(data.shape[1]))
-        ax.set_yticks(np.arange(data.shape[0]))
-        # ... and label them with the respective list entries.
-        ax.set_xticklabels(col_labels)
-        ax.set_yticklabels(row_labels)
-
-        # Let the horizontal axes labeling appear on top.
-        ax.tick_params(top=False, bottom=True,
-                       labeltop=False, labelbottom=True)
-
-        # Rotate the tick labels and set their alignment.
-        plt.setp(ax.get_xticklabels(), rotation=30, ha="right", rotation_mode="anchor")
-
-        # Turn spines off and create white grid.
-        for edge, spine in ax.spines.items():
-            spine.set_visible(False)
-
-        ax.set_xticks(np.arange(data.shape[1] + 1) - .5, minor=True)
-        ax.set_yticks(np.arange(data.shape[0] + 1) - .5, minor=True)
-        ax.grid(which="minor", color="w", linestyle='-', linewidth=5)
-        ax.tick_params(which="minor", bottom=True, left=False)
-
-
-        return im, cbar
-
-    @staticmethod
-    def annotate_heatmap(im, data=None, valfmt="{x:.2f}",
-                         textcolors=["black", "white"],
-                         threshold=None, **textkw):
-        """
-        A function to annotate a heatmap.
-
-        Parameters
-        ----------
-        im
-            The AxesImage to be labeled.
-        data
-            Data used to annotate.  If None, the image's data is used.  Optional.
-        valfmt
-            The format of the annotations inside the heatmap.  This should either
-            use the string format method, e.g. "$ {x:.2f}", or be a
-            `matplotlib.ticker.Formatter`.  Optional.
-        textcolors
-            A list or array of two color specifications.  The first is used for
-            values below a threshold, the second for those above.  Optional.
-        threshold
-            Value in data units according to which the colors from textcolors are
-            applied.  If None (the default) uses the middle of the colormap as
-            separation.  Optional.
-        **kwargs
-            All other arguments are forwarded to each call to `text` used to create
-            the text labels.
-        """
-
-        if not isinstance(data, (list, np.ndarray)):
-            data = im.get_array()
-
-        # Normalize the threshold to the images color range.
-        if threshold is not None:
-            threshold = im.norm(threshold)
-        else:
-            threshold = im.norm(180) / 2.
-
-        # Set default alignment to center, but allow it to be
-        # overwritten by textkw.
-        kw = dict(horizontalalignment="center",
-                  verticalalignment="center")
-        kw.update(textkw)
-
-        # Get the formatter in case a string is supplied
-        if isinstance(valfmt, str):
-            valfmt = matplotlib.ticker.StrMethodFormatter(valfmt)
-
-        # Loop over the data and create a `Text` for each "pixel".
-        # Change the text's color depending on the data.
-        texts = []
-        for i in range(data.shape[0]):
-            for j in range(data.shape[1]):
-                kw.update(color=textcolors[int(im.norm(data[i, j]) > threshold)])
-                text = im.axes.text(j, i, valfmt(data[i, j], None), **kw)
-                texts.append(text)
-
-        return texts
-
     def plot_matrix(self):
         apertures = self.pull_apertures()
         angle_matrix = self.pull_rot_vel_alignment_angles()
@@ -167,26 +46,41 @@ class CorrelationMatrix(pull.FOFRead):
             apertures = apertures[self.aperture_index]
             angle_matrix = angle_matrix[self.aperture_index]
 
-        fig, ax = plt.subplots(figsize=(8, 8))
+        fig = plt.figure(figsize=(12, 12))
+        ax = fig.add_subplot(111)
+
+        norm = colors.LogNorm(vmin=1e-6, vmax=180)
+        im = ax.imshow(angle_matrix, cmap='RdBu', norm=norm, origin='lower')
+        # Manipulate the colorbar on the side
+        divider = make_axes_locatable(ax)
+        cax = divider.append_axes("right", size="3%", pad=0.)
+        cbar = fig.colorbar(im, cax=cax)
+        cbar.ax.minorticks_off()
+        cbar.ax.set_ylabel(r'$y_{rKSZ} \equiv \frac{\Delta T}{T_{CMB}}$', rotation=270, size=25, labelpad=30)
 
         x_labels = [
-            r'$Total~ZMF$',
-            r'$Total~angmom$',
-            r'$ParType0~ZMF$',
-            r'$ParType1~ZMF$',
-            r'$ParType4~ZMF$',
-            r'$ParType5~ZMF$',
-            r'$ParType0~angmom$',
-            r'$ParType1~angmom$',
-            r'$ParType4~angmom$',
-            r'$ParType5~angmom$'
+            r'$\mathbf{\hat{v_{pec}}}$',
+            r'$\mathbf{\hat{L}}$',
+            r'$\mathbf{\hat{v_{pec}^{(gas)}}}$',
+            r'$\mathbf{\hat{v_{pec}^{(DM)}}}$',
+            r'$\mathbf{\hat{v_{pec}^{(stars)}}}$',
+            r'$\mathbf{\hat{v_{pec}^{(BH)}}}$',
+            r'$\mathbf{\hat{L^{(gas)}}}$',
+            r'$\mathbf{\hat{L^{(DM)}}}$',
+            r'$\mathbf{\hat{L^{(stars)}}}$',
+            r'$\mathbf{\hat{L^{(BH)}}}$'
         ]
 
-        im, cbar = self.heatmap(angle_matrix, x_labels, x_labels, ax=ax,
-                                cmap="RdYlGn_r", cbarlabel=r"Misalignment angle  [degrees]")
-        texts = self.annotate_heatmap(im, valfmt=r"{x:.1f}")
-        plt.title(r"Aperture = {:.2f} Mpc".format(apertures), size = 25)
+        ax.set_xticklabels(x_labels)
+        ax.set_yticklabels(x_labels)
 
+        # Loop over data dimensions and create text annotations.
+        for i in range(len(x_labels)):
+            for j in range(len(x_labels)):
+                text = ax.text(j, i, angle_matrix[i, j],
+                               ha="center", va="center", color="k")
+
+        ax.set_title(r"Aperture = {:.2f} Mpc".format(apertures), size = 20)
         fig.tight_layout()
         plt.show()
 
