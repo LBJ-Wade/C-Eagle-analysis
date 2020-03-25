9f7afb86b18b1cf80624037352bbea9a3a6a1e25
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-03-25 16:04:04 +0000

    Test group number

--- Unittest/test_rotvel_alignment.py
+++ Unittest/test_rotvel_alignment.py
@@ -191,12 +191,9 @@ class TestRotVel(unittest.TestCase):
             hd5set = f['/PartType1/GroupNumber']
             group_number = hd5set[...]
             print(f"{' celr_e | halo 0 | z=0 ':-^60}\n")
-            print("Particles with group number < 0", group_number[np.where(group_number<0)[0]],  len(np.where(
-                group_number<0)[0]), sep='\n')
-            print("Particles with group number = 0", group_number[np.where(group_number==0)[0]], len(np.where(
-                group_number==0)[0]), sep='\n')
-            print("Particles with group number = 1", group_number[np.where(group_number==1)[0]], len(np.where(
-                group_number==1)[0]), sep='\n')
+            print(f"Particles with group number < : {len(np.where(group_number<0)[0])} particles found.")
+            print(f"Particles with group number = : {len(np.where(group_number==0)[0])} particles found.")
+            print(f"Particles with group number = : {len(np.where(group_number==1)[0])} particles found.")
 
         # Read in celr_b | halo 0 | z=0
         path = '/cosma5/data/dp004/dc-pear3/data/bahamas/halo_00/data/particledata_029'
@@ -204,12 +201,37 @@ class TestRotVel(unittest.TestCase):
             hd5set = f['/PartType1/GroupNumber']
             group_number = hd5set[...]
             print(f"{' celr_b | halo 0 | z=0 ':-^60}\n")
-            print("Particles with group number < 0", group_number[np.where(group_number < 0)[0]], len(np.where(
-                group_number < 0)[0]), sep='\n')
-            print("Particles with group number = 0", group_number[np.where(group_number == 0)[0]], len(np.where(
-                group_number == 0)[0]), sep='\n')
-            print("Particles with group number = 1", group_number[np.where(group_number == 1)[0]], len(np.where(
-                group_number == 1)[0]), sep='\n')
+            print(f"Particles with group number < 0: {len(np.where(group_number < 0)[0])} particles found.")
+            print(f"Particles with group number = 0: {len(np.where(group_number == 0)[0])} particles found.")
+            print(f"Particles with group number = 1: {len(np.where(group_number == 1)[0])} particles found.")
+
+        # Read in macsis | halo 0 | z=0
+        path = '/cosma5/data/dp004/dc-hens1/macsis/macsis_gas/halo_0000/data/particledata_022'
+        with h5.File(os.path.join(path, 'eagle_subfind_particles_022.0.hdf5'), 'r') as f:
+            hd5set = f['/PartType1/GroupNumber']
+            group_number = hd5set[...]
+            print(f"{' macsis | halo 0 | z=0 ':-^60}\n")
+            print(f"Particles with group number < 0: {len(np.where(group_number < 0)[0])} particles found.")
+            print(f"Particles with group number = 0: {len(np.where(group_number == 0)[0])} particles found.")
+            print(f"Particles with group number = 1: {len(np.where(group_number == 1)[0])} particles found.")
+
+        # Read in ceagle | halo 0 | z=0
+        path = '/cosma5/data/dp004/C-EAGLE/Complete_Sample/CE_00/data/particledata_029_z000p000'
+        group_number = np.zeros(0, dtype=np.int)
+        file_index = 0
+
+        while file_index > 0:
+            try:
+                with h5.File(os.path.join(path, f'eagle_subfind_particles_029_z000p000.{file_index}.hdf5'), 'r') as f:
+                    hd5set = f['/PartType1/GroupNumber']
+                    group_number = np.concatenate((group_number, hd5set[...]), axis=0)
+            except:
+                file_index = -1
+
+            print(f"{' ceagle | halo 0 | z=0 ':-^60}\n")
+            print(f"Particles with group number < 0: {len(np.where(group_number < 0)[0])} particles found.")
+            print(f"Particles with group number = 0: {len(np.where(group_number == 0)[0])} particles found.")
+            print(f"Particles with group number = 1: {len(np.where(group_number == 1)[0])} particles found.")
 
 if __name__ == '__main__':
     unittest.main()
