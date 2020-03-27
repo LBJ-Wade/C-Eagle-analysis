b37c69e377db2c21eedf665962634c8e256d0aa6
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-03-27 20:29:41 +0000

    Update the group methods

--- import_toolkit/cluster.py
+++ import_toolkit/cluster.py
@@ -3,9 +3,9 @@ from typing import List, Dict, Tuple
 import os
 import numpy as np
 
-from . import simulation
-from . import _cluster_retriever
-from . import _cluster_profiler
+import simulation
+import _cluster_retriever
+import _cluster_profiler
 
 class Cluster(simulation.Simulation,
               _cluster_retriever.Mixin,
