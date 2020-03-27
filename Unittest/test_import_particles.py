5e5a27430e3be1cf9f23d69d9e0dc9f24388e4d4
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-03-27 21:11:52 +0000

    Update the group methods

--- Unittest/test_import_particles.py
+++ Unittest/test_import_particles.py
@@ -111,7 +111,7 @@ class TestMixin(unittest.TestCase):
             file_index += 1
 
         print(f"{' SOFTWARE TEST ':=^60}")
-        for sim in ['celr_e', 'celr_b', 'macsis']:
+        for sim in ['celr_e', 'celr_b', 'macsis', 'ceagle']:
             cluster = Cluster(simulation_name=sim, clusterID=0, redshift='z000p000')
             print(f"\n {sim}{' | halo 0 | z=0 ':-^60}")
             # print("cluster.groups_filePaths", cluster.groups_filePaths(), sep='\n')
