5604d8cbce3760920d708cf866950965b6ce68e0
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-03-30 15:56:51 +0100

    Generate rotvel matrices

--- rotvel_correlation/align_angles.py
+++ rotvel_correlation/align_angles.py
@@ -78,13 +78,14 @@ class CorrelationMatrix(pull.FOFRead):
         # Loop over data dimensions and create text annotations.
         for i in range(len(x_labels)):
             for j in range(len(x_labels)):
-                text = ax.text(j, i, r"${:.2f}^{{+{:.2f}}}_{{-{:.2f}}}$".format(
-                                angle_matrix[i, j],
-                                std1_matrix[i, j] - angle_matrix[i, j],
-                                angle_matrix[i, j] - std0_matrix[i, j]),
-                                ha="center", va="center", color="k", size = 15)
+                if i is not j:
+                    text = ax.text(j, i, r"${:.2f}^{{+{:.2f}}}_{{-{:.2f}}}$".format(
+                                    angle_matrix[i, j],
+                                    std1_matrix[i, j] - angle_matrix[i, j],
+                                    angle_matrix[i, j] - std0_matrix[i, j]),
+                                    ha="center", va="center", color="k", size = 13)
 
-        ax.set_title(r"Aperture = {:.2f}".format(apertures) + r"\ $R_{500\ true}$", size = 8)
+        ax.set_title(r"Aperture = {:.2f}".format(apertures) + r"\ $R_{500\ true}$", size = 20)
         ax.grid(b = True, which='minor',color='w', linestyle='-', linewidth=10, alpha = 1)
 
         ax.tick_params(which='minor', length=0, color='w')
