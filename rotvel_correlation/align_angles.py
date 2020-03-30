769229380f5383dc9af2adc03ddcc6b9be11df06
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-03-30 12:58:22 +0100

    Output status report

--- rotvel_correlation/align_angles.py
+++ rotvel_correlation/align_angles.py
@@ -14,6 +14,8 @@ etc.
 import numpy as np
 import sys
 import os.path
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
