7a171dfa46da4ebfa1e7dbdc1388965e57912088
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-03-31 20:22:27 +0100

    Redshift trend

--- rotvel_correlation/align_angles.py
+++ rotvel_correlation/align_angles.py
@@ -170,18 +170,17 @@ class TrendZ:
                       aperture_id: int = 10):
 
         sim = Simulation(simulation_name=simulation_name)
-        print(f"{sim.simulation:=^100s}")
         aperture_id_str = f'Aperture {aperture_id}'
-        print(f"{aperture_id_str:^100s}")
-        print(f"{'  ':<30s} | {' process ID ':^25s} | {' halo ID ':^15s} | "
-              f"{' halo redshift ':^20s}\n")
+        print(f"{sim.simulation:=^100s}")
+        print(f"{aperture_id_str:^100s}\n")
+        print(f"{'':<30s} {' process ID ':^25s} | {' halo ID ':^15s} | {' halo redshift ':^20s}\n")
 
         angle_master = np.zeros((len(sim.redshiftAllowed), len(sim.clusterIDAllowed)), dtype=np.float)
         z_master = np.array([redshift_str2num(z) for z in sim.redshiftAllowed])
 
         iterator = itertools.product(sim.clusterIDAllowed, sim.redshiftAllowed)
         for process_n, (halo_id, halo_z) in enumerate(list(iterator)):
-            print(f"{'Processing...':<30s} | {process_n:^25s} | {halo_id:^15s} | {halo_z:^20s}")
+            print(f"{'Processing...':<30s} | {process_n:^25d} | {halo_id:^15d} | {halo_z:^20s}")
             cluster = Cluster(simulation_name=simulation_name, clusterID=halo_id, redshift=halo_z)
             read = pull.FOFRead(cluster)
             angle = read.pull_rot_vel_angle_between('Total_angmom', 'Total_ZMF')[aperture_id]
