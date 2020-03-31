264d0f7264fe2c6ea77f9c5254539aa427cddc4f
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-03-31 21:10:36 +0100

    Redshift trend

--- rotvel_correlation/align_angles.py
+++ rotvel_correlation/align_angles.py
@@ -175,7 +175,7 @@ class TrendZ:
         print(f"{aperture_id_str:^100s}\n")
         print(f"{'':<30s} {' process ID ':^25s} | {' halo ID ':^15s} | {' halo redshift ':^20s}\n")
 
-        angle_master = np.zeros((len(sim.redshiftAllowed), len(sim.clusterIDAllowed)), dtype=np.float)
+        angle_master = np.zeros(( len(sim.clusterIDAllowed), len(sim.redshiftAllowed)), dtype=np.float)
         print(angle_master.shape)
         print(angle_master)
         z_master = np.array([redshift_str2num(z) for z in sim.redshiftAllowed])
