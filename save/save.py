4797fdf8532e7b10136f254910057d88c87b9549
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-04-02 13:43:58 +0100

    Report update

--- save/save.py
+++ save/save.py
@@ -115,11 +115,7 @@ class SimulationOutput(Simulation):
 
         return ax
 
-    @staticmethod
-    def forceAspect(ax, ax_target, aspect):
-        im = ax.get_images()
-        extent = im[0].get_extent()
-        ax_target.set_aspect(abs((extent[1] - extent[0]) / (extent[3] - extent[2])) / aspect)
+
 
     @ProgressBar()
     def status_plot(self):
@@ -128,6 +124,7 @@ class SimulationOutput(Simulation):
         ax = fig.add_subplot(111)
         divider = make_axes_locatable(ax)
         cax = divider.append_axes("right", size="3%", pad=0.05)
+        cax.set_aspect(1.0)
 
         report_matrix = np.zeros((len(self.clusterIDAllowed), len(self.redshiftAllowed)), dtype=np.int)
         length_operation = np.product(report_matrix.shape)
@@ -154,7 +151,7 @@ class SimulationOutput(Simulation):
                         aspect = report_matrix.shape[1]/report_matrix.shape[0],
                         extent=(-0.1*len(self.redshiftAllowed), 1.1*len(self.redshiftAllowed),
                                 -0.1*self.totalClusters, 1.1*self.totalClusters))
-        self.forceAspect(ax, cax, 1.0)
+
         cbar = fig.colorbar(img, cmap=cmap, cax=cax, norm=norm, boundaries=bounds, ticks=[0, 1, 2, 3, 4, 8, 12, 13])
         cbar.ax.minorticks_off()
         cbar.ax.set_ylabel(r'Number of files in directory', rotation=270, size=25, labelpad=40)
