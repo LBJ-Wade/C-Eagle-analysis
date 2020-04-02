ff66d5e554081363dd173ba38d410d02486cb20d
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-04-02 12:44:00 +0100

    Report update

--- save/save.py
+++ save/save.py
@@ -143,13 +143,14 @@ class SimulationOutput(Simulation):
         bounds = [0, 0.5, 3.5, expected_total_files-0.5, expected_total_files]
         norm   = colors.BoundaryNorm(bounds, cmap.N)
         img = ax.imshow(report_matrix, interpolation='nearest', cmap=cmap, norm=norm, origin='upper',
-                            extent=(-0.1*len(self.redshiftAllowed), 1.1*len(self.redshiftAllowed),
-                                    -0.1*self.totalClusters, 1.1*self.totalClusters))
+                        aspect = report_matrix.shape[1]/report_matrix.shape[0],
+                        extent=(-0.1*len(self.redshiftAllowed), 1.1*len(self.redshiftAllowed),
+                                -0.1*self.totalClusters, 1.1*self.totalClusters))
 
         # Manipulate the colorbar on the side
         divider = make_axes_locatable(ax)
         cax = divider.append_axes("right", size="3%", pad=0.)
-        cbar = fig.colorbar(img, cmap=cmap, norm=norm, boundaries=bounds, ticks=np.arange(0, expected_total_files + 1))
+        cbar = fig.colorbar(img, cmap=cmap, cax=cax, norm=norm, boundaries=bounds, ticks=[0, 1, 2, 3, 4, 8, 12, 13])
         cbar.ax.minorticks_off()
         cbar.ax.set_ylabel(r'Number of files in directory', rotation=270, size=25, labelpad=30)
 
