81f24e69b4208363ffc2c44074c3c915f83511b8
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-04-02 13:47:40 +0100

    Report update

--- save/save.py
+++ save/save.py
@@ -151,7 +151,7 @@ class SimulationOutput(Simulation):
                         aspect = report_matrix.shape[1]/report_matrix.shape[0],
                         extent=(-0.1*len(self.redshiftAllowed), 1.1*len(self.redshiftAllowed),
                                 -0.1*self.totalClusters, 1.1*self.totalClusters))
-        cax.set_aspect(img.get_aspect())
+        cax.set_aspect(report_matrix.shape[1]/report_matrix.shape[0])
         cbar = fig.colorbar(img, cmap=cmap, cax=cax, norm=norm, boundaries=bounds, ticks=[0, 1, 2, 3, 4, 8, 12, 13])
         cbar.ax.minorticks_off()
         cbar.ax.set_ylabel(r'Number of files in directory', rotation=270, size=25, labelpad=40)
