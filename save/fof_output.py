98d6f507275a7f98a1aa14b4a5b94b4ae53a0f93
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-04-04 15:23:28 +0100

    z-trends

--- save/fof_output.py
+++ save/fof_output.py
@@ -689,7 +689,7 @@ if __name__ == '__main__':
             out.push_peculiar_velocity()
             out.push_angular_momentum()
         """
-        for sim_name in ['ceagle']:
+        for sim_name in ['macsis']:
 
             # Set-up the MPI allocation schedule
             sim = Simulation(simulation_name=sim_name)
