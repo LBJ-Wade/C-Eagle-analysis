21bcc75214db9946136df9b6beac20ab75268bfb
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-04-05 12:08:39 +0100

    z-trends

--- rotvel_correlation/align_angles.py
+++ rotvel_correlation/align_angles.py
@@ -234,9 +234,9 @@ class TrendZ:
         #             markersize=5,
         #             marker='o',
         #             capsize=5)
-        ax.step(z_master, percent84, color = 'lime', alpha = 1, step='mid', linestyle='--')
-        ax.step(z_master, median50, color = 'lime', alpha = 1, step='mid', linestyle='-')
-        ax.step(z_master, percent16, color = 'lime', alpha = 1, step='mid', linestyle='--.')
+        ax.plot(z_master, percent84, color = 'lime', alpha = 1, drawstyle='steps-mid', linestyle='--')
+        ax.plot(z_master, median50, color = 'lime', alpha = 1,  drawstyle='steps-mid', linestyle='-')
+        ax.plot(z_master, percent16, color = 'lime', alpha = 1, drawstyle='steps-mid', linestyle='--.')
         ax.fill_between(z_master, percent84 - error, percent84 + error, color = 'lime', alpha = 0.3, step='mid')
         ax.fill_between(z_master, median50 - error,  median50 + error,  color = 'lime', alpha = 0.3, step='mid')
         ax.fill_between(z_master, percent16 - error, percent16 + error, color = 'lime', alpha = 0.3, step='mid')
