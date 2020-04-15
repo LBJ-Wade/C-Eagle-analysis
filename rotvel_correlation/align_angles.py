203f3f15c349fcc83d01a11f397aa20f576e81a0
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-04-15 12:41:23 +0100

    z-trend update polar

--- rotvel_correlation/align_angles.py
+++ rotvel_correlation/align_angles.py
@@ -538,8 +538,8 @@ class TrendZ:
 			# inset_axis.annotate(r"$\mathbf{L}$", xytext=(0, arrow_radius), xy=(0, 0), arrowprops=kw)
 			# inset_axis.annotate(r"$\mathbf{v_{pec}}$", xytext=(np.pi/2, arrow_radius), xy=(0, 0), arrowprops=kw)
 
-			inset_axis.arrow(0, 0, 0, arrow_radius, alpha=0.5, width=0.015, edgecolor='black', facecolor='black', lw=2, zorder=5)
-			inset_axis.arrow(0, 0, np.pi/2, arrow_radius, alpha=0.5, width=0.015, edgecolor='black', facecolor='black', lw=2, zorder=5)
+			inset_axis.arrow(0, 0, 0, arrow_radius, alpha=0.5, width=0.15, edgecolor='black', facecolor='black', lw=20, zorder=5)
+			inset_axis.arrow(0, 0, np.pi/2, arrow_radius, alpha=0.5, width=0.15, edgecolor='black', facecolor='black', lw=20, zorder=5)
 
 
 	def save_z_trend(self, common_folder: bool = False) -> None:
