950c5233432ab9b5ac371af1784b87d1f48a4546
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-04-23 20:02:32 +0100

    Simstats beta

--- rotvel_correlation/simstats.py
+++ rotvel_correlation/simstats.py
@@ -234,4 +234,4 @@ if __name__ == '__main__':
 	simstats = Simstats(simulation_name='celr_b', aperture_id=10)
 	simstats.make_simstats(save2hdf5=True)
 	stats_out = pd.read_hdf(os.path.join(simstats.path, 'simstats_celr_b_aperture10.h5'))
-	print(stats_out['redshift_float':'vel0vel1'])
+	print(stats_out)
