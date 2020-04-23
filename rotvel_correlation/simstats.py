10e625dc737878eb256f732728752f527ef3d3b2
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-04-23 19:49:27 +0100

    Simstats beta

--- rotvel_correlation/simstats.py
+++ rotvel_correlation/simstats.py
@@ -84,8 +84,7 @@ class Simstats:
 		return np.sqrt(bins[:-1] * bins[1:])
 
 	def make_simstats(self, save2hdf5: bool = True) -> pd.DataFrame:
-		assert self.simulation is not None
-		iterator = itertools.product(self.simulation.clusterIDAllowed, self.simulation.redshiftAllowed)
+
 		cols = [
 				'cluster_id',
 				'redshift_float',
@@ -124,7 +123,6 @@ class Simstats:
 		labels_tex = [
 				r'Cluster ID',
 				r'Redshift',
-
 				r'$R_{2500,crit}$\quad[Mpc]',
 				r'$R_{500,crit}$\quad[Mpc]',
 				r'$R_{200,crit}$\quad[Mpc]',
@@ -161,7 +159,7 @@ class Simstats:
 				'Simulation' : self.simulation.simulation,
 				'Number of clusters' : self.simulation.totalClusters,
 				'Redshift bounds' : '0 - 1.8',
-				'Sample completeness' : np.sum(self.simulation.sample_completeness)/len(list(iterator)),
+				'Sample completeness' : np.sum(self.simulation.sample_completeness)/np.product(self.simulation.sample_completeness.shape),
 				'Aperture radius'   : self.aperture_id,
 				'z mL equality' : self.get_matterLambda_equality_z(),
 				'Cluster centre reference' : 'Centre of potential',
@@ -170,9 +168,9 @@ class Simstats:
 		}
 
 		df = pd.DataFrame(columns=cols)
+		iterator = itertools.product(self.simulation.clusterIDAllowed, self.simulation.redshiftAllowed)
 		print(f"{'':<30s} {' process ID ':^25s} | {' halo ID ':^15s} | {' halo redshift ':^20s}\n")
 		for process_n, (halo_id, halo_z) in enumerate(list(iterator)):
-			print('k')
 			if self.simulation.sample_completeness[halo_id, self.simulation.redshiftAllowed.index(halo_z)]:
 				print(f"{'Processing...':<30s} {process_n:^25d} | {halo_id:^15d} | {halo_z:^20s}")
 				cluster = Cluster(simulation_name=self.simulation.simulation_name,
@@ -180,7 +178,7 @@ class Simstats:
 								  redshift=halo_z)
 				read = pull.FOFRead(cluster)
 				df = df.append({
-					'cluster_id' : cluster.clusterID,
+					'cluster_id'     : cluster.clusterID,
 					'redshift_float' : cluster.z,
 					'R_2500_crit'  : cluster.r2500,
 					'R_500_crit'   : cluster.r500,
@@ -205,14 +203,14 @@ class Simstats:
 					'angular_momentum_0_magnitude' : cluster.momentum_units(read.pull_angular_momentum_magnitude('ParType0_angmom')[self.aperture_id], unit_system='astro'),
 					'angular_momentum_1_magnitude' : cluster.momentum_units(read.pull_angular_momentum_magnitude('ParType1_angmom')[self.aperture_id], unit_system='astro'),
 					'angular_momentum_4_magnitude' : cluster.momentum_units(read.pull_angular_momentum_magnitude('ParType4_angmom')[self.aperture_id], unit_system='astro'),
-					'kinetic_energy_T' : cluster.energy_units(read.pull_kinetic_energy('Total_kin_energy')[self.aperture_id], unit_system='SI'),
-					'kinetic_energy_0' : cluster.energy_units(read.pull_kinetic_energy('ParType0_kin_energy')[self.aperture_id], unit_system='SI'),
-					'kinetic_energy_1' : cluster.energy_units(read.pull_kinetic_energy('ParType1_kin_energy')[self.aperture_id], unit_system='SI'),
-					'thermal_energy'   : cluster.energy_units(read.pull_thermal_energy('Total_th_energy')[self.aperture_id], unit_system='SI'),
+					'kinetic_energy_T'    : cluster.energy_units(read.pull_kinetic_energy('Total_kin_energy')[self.aperture_id], unit_system='SI'),
+					'kinetic_energy_0'    : cluster.energy_units(read.pull_kinetic_energy('ParType0_kin_energy')[self.aperture_id], unit_system='SI'),
+					'kinetic_energy_1'    : cluster.energy_units(read.pull_kinetic_energy('ParType1_kin_energy')[self.aperture_id], unit_system='SI'),
+					'thermal_energy'      : cluster.energy_units(read.pull_thermal_energy('Total_th_energy')[self.aperture_id], unit_system='SI'),
 					'substructure_mass_T' : cluster.mass_units(read.pull_substructure_mass('Total_substructure_mass')[self.aperture_id], unit_system='astro'),
-					'dynamical_merging_index_T' : read.pull_dynamical_merging_index('Total_dyn_mergindex')[self.aperture_id],
+					'dynamical_merging_index_T'     : read.pull_dynamical_merging_index('Total_dyn_mergindex')[self.aperture_id],
 					'thermodynamic_merging_index_T' : read.pull_thermodynamic_merging_index('Total_therm_mergindex')[self.aperture_id],
-					'substructure_fraction_T' : read.pull_substructure_merging_index('Total_substructure_fraction')[self.aperture_id]
+					'substructure_fraction_T'       : read.pull_substructure_merging_index('Total_substructure_fraction')[self.aperture_id]
 				}, ignore_index=True)
 			else:
 				print(f"{'Skip - sample_completeness':<30s} {process_n:^25d} | {halo_id:^15d} | {halo_z:^20s}")
