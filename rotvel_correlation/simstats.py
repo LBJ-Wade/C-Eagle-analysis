1f6bab11bb88652881744b075b54ed01c64b8622
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-04-25 10:54:41 +0100

    Simstats beta

--- rotvel_correlation/simstats.py
+++ rotvel_correlation/simstats.py
@@ -250,8 +250,8 @@ class Simstats:
 if __name__ == '__main__':
 
 	simstats = Simstats(simulation_name='celr_b', aperture_id=10)
-	simstats.make_simstats(save2hdf5=True)
 	simstats.clear_file()
+	simstats.make_simstats(save2hdf5=True)
 	stats_out = simstats.read_simstats()
 	print(stats_out.query('cluster_id == 0 and redshift_float < 0.1')['redshift_float'])
 	print(simstats.read_metadata())
