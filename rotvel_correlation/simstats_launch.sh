67cf9ae78cd8a3785cce0eeb1c3e395f66af099a
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-04-25 13:06:15 +0100

    Simstats launch

--- rotvel_correlation/simstats_launch.sh
+++ rotvel_correlation/simstats_launch.sh
@@ -4,7 +4,7 @@ echo -e "\e[91mRemoving outdated SLURM output records...\e[0m"
 rm standard_*
 
 echo -e "\e[1m\e[91mSubmitting script to SLURM...\e[0m"
-sbatch simstats_schedule.sh
+sbatch ./simstats_schedule.sh
 
 echo -e "\e[5m\e[1m\e[91mJob submitted.\e[0m"
 squeue -u dc-alta2
