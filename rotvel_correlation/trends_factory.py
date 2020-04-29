d220df5c9bfe0587b000218f3706f96291c67b69
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-04-29 17:54:40 +0100

    Trends factory make

--- rotvel_correlation/trends_factory.py
+++ rotvel_correlation/trends_factory.py
@@ -180,7 +180,7 @@ for entry_index, data_entry in enumerate(data_entries):
             $z$ = %s 
             Aperture radius = %2.2f $R_{200\ true}$""".format(*items_info)
 
-    info_ax0.text(0.03, 0.97, items_labels, horizontalalignment='left', verticalalignment='top', size=15)
+    info_ax0.text(0.03, 0.97, items_labels, horizontalalignment='left', verticalalignment='top', size=15, transform=info_ax0.transAxes)
     ax[0].set_ylabel(label_y)
     ax[1].set_ylabel(label_y)
     ax[1].set_xlabel(label_x)
