bc26de4c5963297aaf541ee5578518019f7230fe
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-04-30 21:08:34 +0100

    Trends factory make

--- rotvel_correlation/trends_factory.py
+++ rotvel_correlation/trends_factory.py
@@ -113,7 +113,7 @@ simstats.append(Simstats(simulation_name='celr_e', aperture_id=aperture_id))
 simstats.append(Simstats(simulation_name='celr_b', aperture_id=aperture_id))
 stats_out = [sim.read_simstats() for sim in simstats]
 attrs = [sim.read_metadata() for sim in simstats]
-print(f"\n{' stats_out DATASET INFO ':^-40s}", stats_out[0].info())
+print(f"\n{' stats_out DATASET INFO ':-^40s}", stats_out[0].info())
 
 # Create SQL query
 query_COLLECTIVE = list()
@@ -153,12 +153,12 @@ data_summary = {
 summary = pd.DataFrame(data=data_summary, columns=data_summary.keys())
 summary = summary[summary['y'].str.contains('rot')]
 summary = summary[~summary['x'].str.contains('redshift')]
-print(f"\n{' summary DATASET PLOTS INFO ':^-40s}", summary)
+print(f"\n{' summary DATASET PLOTS INFO ':-^40s}", summary)
 
 
 
 # Activate the plot factory
-print(f"\n{' RUNNING PLOT FACTORY... ':^-40s}")
+print(f"\n{' RUNNING PLOT FACTORY... ':^--0s}")
 data_entries = summary.to_dict('r')
 for entry_index, data_entry in enumerate(data_entries):
     filename = f"_{data_entry['x'].replace('_', '')}_{data_entry['y'].replace('_', '')}_aperture"f"{aperture_id}.png"
