03305ca5b990f4f4c44e374b6f874f0677830199
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-04-30 20:02:25 +0100

    Trends factory make

--- rotvel_correlation/trends_factory.py
+++ rotvel_correlation/trends_factory.py
@@ -181,10 +181,11 @@ for entry_index, data_entry in enumerate(data_entries):
     ax[3].set_xlabel(label_x)
     simstats_palette = ['#1B9E77','#D95F02','#7570B3','#E7298A']
 
-    items_labels = f"""{label_x.split('[')[0].strip('quad')} \\textemdash\\ {label_y.split('[')[0]}
+    items_labels = f"""{label_x.split('quad')[0]} \\textemdash\\ {label_y.split('quad')[0]}
         Number of clusters: {np.sum([attr['Number of clusters'] for attr in attrs])}
         $z$ = {attrs[0]['Redshift bounds']}
         Aperture radius = {stats_filtered[0]['R_aperture'][0] / stats_filtered[0]['R_200_crit'][0]:2.2f} $R_{{200\ true}}$"""
+    print(items_labels)
     info_ax0.text(0.03, 0.97, items_labels, horizontalalignment='left', verticalalignment='top', size=15, transform=info_ax0.transAxes)
 
     axisinfo_kwargs = dict(
