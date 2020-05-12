684d27e50d3d4b994329d7c82db5a95cba8ab436
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-05-12 17:58:49 +0100

    bahamas box scrape

--- testing/bahamaspartmap.py
+++ testing/bahamaspartmap.py
@@ -33,12 +33,11 @@ ax.set_ylabel(r'$z\ $ [Mpc]')
 
 coords = cluster.particle_coordinates('gas')
 x = coords[:,0]
-# y = coords[:,1]
-z = coords[:,2]
+y = coords[:,1]
 del coords
 ax.set_xlim(np.min(x), np.max(x))
-ax.set_ylim(np.min(z), np.max(z))
-ax.scatter(x,z, marker=',', c='k', alpha=0.01)
+ax.set_ylim(np.min(y), np.max(y))
+ax.scatter(x,y, marker=',', c='k', alpha=0.01)
 
 items_labels = r"""POINT PARTICLE MAP
 Cluster {:s} {:d}
