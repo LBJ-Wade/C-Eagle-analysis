ade8caa6bb90135dc50af475325e11ee9d37354e
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-05-22 15:39:05 +0100

    Ghost implementation

--- import_toolkit/simulation.py
+++ import_toolkit/simulation.py
@@ -263,7 +263,7 @@ class Ghost:
         tag_name = f"Tagger: {tag_label:<15s}"
         mem_name = f"Memory: {mem_label:<15s}"
         tag_size = f"Size in memory: {sys.getsizeof(self.tagger):>10.0f} Bytes"
-        mem_size = f"Size in memory: {sys.getsizeof(self.memory):>10.0f} Bytes"
+        mem_size = f"Size in memory: {sys.getsizeof(self.memory.values()):>10.0f} Bytes"
         print(f"{' Ghost class report ':-^100s}")
         print(f"{tag_name:<60s}{tag_size:<40s}")
         print(f"{mem_name:<60s}{mem_size:<40s}")
