038e628ae310631a7e7ef84f052fe4a7529fc6a2
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-05-22 18:33:37 +0100

    Ghost implementation

--- main.py
+++ main.py
@@ -69,71 +69,72 @@ def main():
     import sys
     import os
     from import_toolkit.simulation import Simulation
+    from import_toolkit.simulation import get_size
     from import_toolkit.cluster import Cluster
     from import_toolkit._cluster_retriever import redshift_str2num
     from rotvel_correlation import alignment
 
-    def bahamas_mass_cut():
-        cluster = Cluster(simulation_name='bahamas',
-                        clusterID=0,
-                        redshift='z000p000',
-                        comovingframe=False,
-                        fastbrowsing=True)
-        n_largeM = 0
-        n_total = 0
-        gn_tot = np.zeros(0, dtype=np.int)
-        N_halos = 0
-        for counter, file in enumerate(cluster.groups_filePaths()):
-            print(f"[+] Analysing eagle_subfind_tab file {counter}")
-            with h5py.File(file, 'r') as group_file:
-                m500 = group_file['/FOF/Group_M_Crit500'][:] * 10 ** 10
-                n_total += len(m500)
-                m_filter = np.where(m500 > 10 ** 13)[0] + N_halos
-                gn_tot = np.append(gn_tot, m_filter)
-                n_largeM += len(m_filter)
-                N_halos += group_file['Header'].attrs['Ngroups']
-
-        print('n_largeM:', n_largeM)
-        print('n_total:', n_total)
-        print('N_halos:', N_halos)
-        print('gn_tot:', gn_tot)
-        np.save(f'import_toolkit/bahamas_fofnumber_list_10--13.npy', gn_tot)
-
-    bahamas_mass_cut()
-
-        # def call_cluster(i):
-    #     data_required = {
-    #             'partType0': ['groupnumber', 'subgroupnumber', 'mass', 'coordinates', 'velocity', 'temperature', 'sphdensity'],
-    #             'partType1': ['groupnumber', 'subgroupnumber', 'mass', 'coordinates', 'velocity'],
-    #             'partType4': ['groupnumber', 'subgroupnumber', 'mass', 'coordinates', 'velocity']
-    #     }
-    #
+    # def bahamas_mass_cut():
     #     cluster = Cluster(simulation_name='bahamas',
-    #                       clusterID=i,
-    #                       redshift='z003p000',
-    #                       comovingframe=False,
-    #                       requires=data_required)
+    #                     clusterID=0,
+    #                     redshift='z000p000',
+    #                     comovingframe=False,
+    #                     fastbrowsing=True)
+    #     n_largeM = 0
+    #     n_total = 0
+    #     gn_tot = np.zeros(0, dtype=np.int)
+    #     N_halos = 0
+    #     for counter, file in enumerate(cluster.groups_filePaths()):
+    #         print(f"[+] Analysing eagle_subfind_tab file {counter}")
+    #         with h5py.File(file, 'r') as group_file:
+    #             m500 = group_file['/FOF/Group_M_Crit500'][:] * 10 ** 10
+    #             n_total += len(m500)
+    #             m_filter = np.where(m500 > 10 ** 13)[0] + N_halos
+    #             gn_tot = np.append(gn_tot, m_filter)
+    #             n_largeM += len(m_filter)
+    #             N_halos += group_file['Header'].attrs['Ngroups']
     #
-    #     apertures = cluster.generate_apertures()
-    #     master_dict = {}
-    #     for i,r_a in enumerate(apertures):
-    #         halo_output = {
-    #                 **cluster.group_fofinfo(aperture_radius=cluster.r200),
-    #                 **cluster.group_dynamics(aperture_radius=cluster.r200),
-    #                 **cluster.group_morphology(aperture_radius=cluster.r200)
-    #         }
-    #         alignment_dict = alignment.group_alignment(halo_output)
-    #         halo_output = {
-    #                 **halo_output,
-    #                 **alignment_dict
-    #         }
-    #         master_dict[f'aperture{i:02d}'] = halo_output
-    #         del halo_output, alignment_dict
-    #     print(f"\nmaster_dict: \tOutput size {sys.getsizeof(master_dict) / 1024:2.0f} kB")
-    #     for key in master_dict: print(key)
+    #     print('n_largeM:', n_largeM)
+    #     print('n_total:', n_total)
+    #     print('N_halos:', N_halos)
+    #     print('gn_tot:', gn_tot)
+    #     np.save(f'import_toolkit/bahamas_fofnumber_list_10--13.npy', gn_tot)
     #
-    # for i in range(12):
-    #     call_cluster(i)
+    # bahamas_mass_cut()
+
+    def call_cluster(i):
+        data_required = {
+                'partType0': ['groupnumber', 'subgroupnumber', 'mass', 'coordinates', 'velocity', 'temperature', 'sphdensity'],
+                'partType1': ['groupnumber', 'subgroupnumber', 'mass', 'coordinates', 'velocity'],
+                'partType4': ['groupnumber', 'subgroupnumber', 'mass', 'coordinates', 'velocity']
+        }
+
+        cluster = Cluster(simulation_name='bahamas',
+                          clusterID=i,
+                          redshift='z000p000',
+                          comovingframe=False,
+                          requires=data_required)
+
+        apertures = cluster.generate_apertures()
+        master_dict = {}
+        for i,r_a in enumerate(apertures):
+            halo_output = {
+                    **cluster.group_fofinfo(aperture_radius=cluster.r200),
+                    **cluster.group_dynamics(aperture_radius=cluster.r200),
+                    **cluster.group_morphology(aperture_radius=cluster.r200)
+            }
+            alignment_dict = alignment.group_alignment(halo_output)
+            halo_output = {
+                    **halo_output,
+                    **alignment_dict
+            }
+            master_dict[f'aperture{i:02d}'] = halo_output
+            del halo_output, alignment_dict
+        print(f"\nmaster_dict: \tOutput size {get_size(master_dict) / 1024:2.0f} kB")
+        for key in master_dict: print(key)
+
+    for i in range(12):
+        call_cluster(i)
 
 
     # def check_dirs(self) -> np.ndarray:
