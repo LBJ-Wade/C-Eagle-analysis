8c78e9e84c2fe705b2fc1204754a1b15d1ce551e
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-06-04 16:47:38 +0100

    Volumes restructuring

--- mpi_mizar.sh
+++ mpi_mizar.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 > access.log
 #/usr/local/openmpi-2.0.1-intel/bin/mpiexec -n 12 python -u ./main.py > ./main.log &
-/usr/local/openmpi-2.0.1-intel/bin/mpiexec -n 12 python3  ./main.py
+/usr/local/openmpi-2.0.1-intel/bin/mpiexec -n 3 python3  ./main.py
