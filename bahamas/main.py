7c506e169c8e7ad2d1b2178d94ead063eb015259
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-06-06 01:29:01 +0100

    MPI and CSRM

--- bahamas/main.py
+++ bahamas/main.py
@@ -17,9 +17,9 @@ def main():
     files = find_files(REDSHIFT)
     header = fof_header(files)
     fof_groups = fof_groups(files, header)
-    # snap_groupnumbers = snap_groupnumbers(files, fof_groups)
-    # cluster_particles = cluster_particles(files, groupNumbers=snap_groupnumbers)
     pprint(fof_groups.keys())
+    snap_groupnumbers = snap_groupnumbers(files, fof_groups)
+    # cluster_particles = cluster_particles(files, groupNumbers=snap_groupnumbers)
     comm.Barrier()
 
 
