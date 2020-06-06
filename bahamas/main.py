de5554aedee43d05f43067e183f2ccc44fd0ed3d
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-06-06 01:26:50 +0100

    MPI and CSRM

--- bahamas/main.py
+++ bahamas/main.py
@@ -1,6 +1,7 @@
-
+import warnings
 from mpi4py import MPI
 from .__init__ import pprint
+warnings.filterwarnings("ignore")
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 nproc = comm.Get_size()
@@ -18,7 +19,8 @@ def main():
     fof_groups = fof_groups(files, header)
     # snap_groupnumbers = snap_groupnumbers(files, fof_groups)
     # cluster_particles = cluster_particles(files, groupNumbers=snap_groupnumbers)
-    print(fof_groups.keys())
+    pprint(fof_groups.keys())
+    comm.Barrier()
 
 
 
