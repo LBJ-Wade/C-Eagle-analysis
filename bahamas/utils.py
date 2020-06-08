589c0c89ae8512cb4bae338101f4e49ab4636085
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-06-08 23:35:36 +0100

    MPI and CSRM

--- bahamas/utils.py
+++ bahamas/utils.py
@@ -48,10 +48,10 @@ def display_benchmarks(redshift: str):
 		for i in range(int(np.max(lines[0]))):
 			idx = np.where(lines[0] == i)[0]
 			if len(idx) == 1:
-				n_fit.append(lines[0][idx])
+				n_fit.append(lines[0][idx]+1)
 				dat_fit.append(lines[0][idx])
 			elif len(idx) > 1:
-				n_fit.append(lines[0][idx[0]])
+				n_fit.append(np.mean(lines[0][idx])+1)
 				dat_fit.append(np.median(lines[0][idx]))
 
 		print(n_fit , dat_fit )
