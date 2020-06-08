335b0991b0d8c4235ee269590cfb029b8a042806
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-06-09 00:09:46 +0100

    MPI and CSRM

--- bahamas/utils.py
+++ bahamas/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import datetime
 from matplotlib import pyplot as plt
 from scipy.optimize import curve_fit
 import slack
@@ -35,14 +36,12 @@ def display_benchmarks(redshift: str):
 		ax = fig.add_subplot(111)
 		ax.set_xscale("log")
 		ax.set_yscale("log")
-		ax.set_xlim(1, 1e5)
+		ax.set_xlim(1, 14400)
 		ax.set_ylim(1e-2, 15)
 		ax.set_xlabel('FOF cluster index')
 		ax.set_ylabel('Computation time [seconds]')
 
 		lines = np.loadtxt(timing_filename, comments="#", delimiter=",", unpack=False).T
-		ax.scatter(lines[0]+1, lines[1], marker = '.', s = 2, alpha=0.5, label=f'z = {redshift_str2num(redshift)}')
-
 
 		# Fit function to benchmarks
 		n_fit = []
@@ -59,8 +58,13 @@ def display_benchmarks(redshift: str):
 		n_fit = np.log10(np.asarray(n_fit))
 		dat_fit = np.log10(np.asarray(dat_fit))
 		fitParams, _ = curve_fit(fitFunc, n_fit, dat_fit)
-		n_display = np.logspace(0, 4.5, 10)
+		n_display = np.logspace(0, np.log10(14400), 10)
 		ax.plot(n_display, 10**fitFunc(np.log10(n_display), fitParams[0], fitParams[1]), color='red')
+
+		# Compute total computing time estimate
+		time_tot = np.sum(10 ** fitFunc(np.log10(np.linspace(1,14401,14401, dtype=np.int)), fitParams[0], fitParams[1]))
+		time_tot = datetime.timedelta(seconds=time_tot)
+		ax.scatter(lines[0] + 1, lines[1], marker='.', s=3, alpha=0.5, label=f'z = {redshift_str2num(redshift)} ETA = {time_tot}')
 
 		plt.legend()
 		plt.savefig(plot_filename, dpi=300)
