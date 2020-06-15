0ee433da3319a34a6bb8a942139f5fb5917d1ac5
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-06-15 12:25:49 +0100

    MPI and CSRM

--- rotvel_correlation/bahamas_trends.py
+++ rotvel_correlation/bahamas_trends.py
@@ -89,7 +89,7 @@ c_l = []
 m500 = []
 
 with h5py.File(os.path.join(basepath, snapname), 'r') as file:
-	for i in range(14400):
+	for i in range(14366):
 		if f'halo_{i:05d}' in file:
 			c_l.append(file[f'halo_{i:05d}/aperture{aperture:02d}/c_l'][1])
 			m500.append(file[f'halo_{i:05d}/aperture{aperture:02d}/m500'][()])
