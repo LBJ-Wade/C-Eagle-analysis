2dd30a529b75861d7fda8c96ab69d482e83ee410
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-06-18 13:30:37 +0100

    MPI and CSRM

--- rotvel_correlation/utils.py
+++ rotvel_correlation/utils.py
@@ -74,77 +74,67 @@ def output_as_pandas(redshift: str, apertureID: int = 7) -> pd.DataFrame:
 	del snap_dict
 	return snap_pd
 
-def bayesian_blocks(t: np.ndarray):
-    """Bayesian Blocks Implementation
-
-    By Jake Vanderplas.  License: BSD
+def bayesian_blocks(t: np.ndarray) -> np.ndarray:
+	"""
+	Bayesian Blocks Implementation
+	By Jake Vanderplas.  License: BSD
     Based on algorithm outlined in http://adsabs.harvard.edu/abs/2012arXiv1207.5578S
-
-    Parameters
-    ----------
-    t : ndarray, length N
-        data to be histogrammed
-
-    Returns
-    -------
-    bins : ndarray
-        array containing the (N+1) bin edges
-
-    Notes
-    -----
     This is an incomplete implementation: it may fail for some
     datasets.  Alternate fitness functions and prior forms can
     be found in the paper listed above.
-    """
-    # copy and sort the array
-    t = np.sort(t)
-    N = len(t)
+	:param t: ndarray, length N data to be histogrammed
+	:return: ndarray array containing the (N+1) bin edges
+	"""
 
-    # create length-(N + 1) array of cell edges
-    edges = np.concatenate([t[:1],
-                            0.5 * (t[1:] + t[:-1]),
-                            t[-1:]])
-    block_length = t[-1] - edges
+	# copy and sort the array
+	t = np.sort(t)
+	N = len(t)
 
-    # arrays needed for the iteration
-    nn_vec = np.ones(N)
-    best = np.zeros(N, dtype=float)
-    last = np.zeros(N, dtype=int)
+	# create length-(N + 1) array of cell edges
+	edges = np.concatenate([t[:1],
+	                        0.5 * (t[1:] + t[:-1]),
+	                        t[-1:]])
+	block_length = t[-1] - edges
 
-    #-----------------------------------------------------------------
-    # Start with first data cell; add one cell at each iteration
-    #-----------------------------------------------------------------
-    for K in range(N):
-        # Compute the width and count of the final bin for all possible
-        # locations of the K^th changepoint
-        width = block_length[:K + 1] - block_length[K + 1]
-        count_vec = np.cumsum(nn_vec[:K + 1][::-1])[::-1]
+	# arrays needed for the iteration
+	nn_vec = np.ones(N)
+	best = np.zeros(N, dtype=float)
+	last = np.zeros(N, dtype=int)
 
-        # evaluate fitness function for these possibilities
-        fit_vec = count_vec * (np.log(count_vec) - np.log(width))
-        fit_vec -= 4  # 4 comes from the prior on the number of changepoints
-        fit_vec[1:] += best[:K]
+	#-----------------------------------------------------------------
+	# Start with first data cell; add one cell at each iteration
+	#-----------------------------------------------------------------
+	for K in range(N):
+		# Compute the width and count of the final bin for all possible
+		# locations of the K^th changepoint
+		width = block_length[:K + 1] - block_length[K + 1]
+		count_vec = np.cumsum(nn_vec[:K + 1][::-1])[::-1]
 
-        # find the max of the fitness: this is the K^th changepoint
-        i_max = np.argmax(fit_vec)
-        last[K] = i_max
-        best[K] = fit_vec[i_max]
+		# evaluate fitness function for these possibilities
+		fit_vec = count_vec * (np.log(count_vec) - np.log(width))
+		fit_vec -= 4  # 4 comes from the prior on the number of changepoints
+		fit_vec[1:] += best[:K]
 
-    #-----------------------------------------------------------------
-    # Recover changepoints by iteratively peeling off the last block
-    #-----------------------------------------------------------------
-    change_points =  np.zeros(N, dtype=int)
-    i_cp = N
-    ind = N
-    while True:
-        i_cp -= 1
-        change_points[i_cp] = ind
-        if ind == 0:
-            break
-        ind = last[ind - 1]
-    change_points = change_points[i_cp:]
+		# find the max of the fitness: this is the K^th changepoint
+		i_max = np.argmax(fit_vec)
+		last[K] = i_max
+		best[K] = fit_vec[i_max]
 
-    return edges[change_points]
+	#-----------------------------------------------------------------
+	# Recover changepoints by iteratively peeling off the last block
+	#-----------------------------------------------------------------
+	change_points =  np.zeros(N, dtype=int)
+	i_cp = N
+	ind = N
+	while True:
+		i_cp -= 1
+		change_points[i_cp] = ind
+		if ind == 0:
+			break
+		ind = last[ind - 1]
+	change_points = change_points[i_cp:]
+
+	return edges[change_points]
 
 def freedman_diaconis(x: np.ndarray) -> np.ndarray:
     """
