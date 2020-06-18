755b3fab38907381eb8299fda0c82a3c1e3ddeef
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-06-18 21:48:57 +0100

    Plotting stats

--- rotvel_correlation/bahamas_trends.py
+++ rotvel_correlation/bahamas_trends.py
@@ -117,9 +117,10 @@ if __name__ == '__main__':
 	redshift = 'z001p000'
 	aperture = 7
 	x_dataset = 'm500'
-	y_dataset = 'b_l'
+	y_dataset = 'a_l'
 	ptype = (2,2)
 	axscales = ['log', 'linear']
+	# Remember to change the dataset slicing as appropriate to the dataset
 	#-----------------------------------------------------------------
 
 	x = utils.read_snap_output(redshift, apertureID=aperture, dataset=x_dataset)[:]
