fadf2c43f6cd935031a22985149c9775829a7331
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-06-18 12:28:29 +0100

    MPI and CSRM

--- rotvel_correlation/bahamas_trends.py
+++ rotvel_correlation/bahamas_trends.py
@@ -8,6 +8,7 @@ import socket
 # Graphics packages
 import matplotlib
 matplotlib.use('Agg')
+from matplotlib.lines import Line2D
 import matplotlib.pyplot as plt
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
@@ -28,7 +29,6 @@ def save_plot(filepath: str, to_slack: bool = False, **kwargs) -> None:
 	"""
 	plt.savefig(filepath, **kwargs)
 	if to_slack:
-		# Send files to Slack: init slack client with access token
 		print(
 				"[+] Forwarding to the `#personal` Slack channel",
 				f"\tDir: {os.path.dirname(filepath)}",
@@ -38,12 +38,32 @@ def save_plot(filepath: str, to_slack: bool = False, **kwargs) -> None:
 		slack_token = 'xoxp-452271173797-451476014913-1101193540773-57eb7b0d416e8764be6849fdeda52ce8'
 		slack_msg = f"Host: {socket.gethostname()}\nDir: {os.path.dirname(filepath)}\nFile: {os.path.basename(filepath)}"
 		try:
+			# Send files to Slack: init slack client with access token
 			client = slack.WebClient(token=slack_token)
 			client.files_upload(file=filepath, initial_comment=slack_msg, channels='#personal')
 		except:
 			warnings.warn("[-] Failed to broadcast plot to Slack channel.")
 
+def median_plot(axes: plt.Axes, *args, **kwargs):
+	"""
 
+	:param axes:
+	:param args:
+	:param kwargs:
+	:return:
+	"""
+	perc84 = Line2D([], [], color='k', marker='^', linestyle='-.', markersize=12, label=r'$84^{th}$ percentile')
+	perc50 = Line2D([], [], color='k', marker='o', linestyle='-', markersize=12, label=r'median')
+	perc16 = Line2D([], [], color='k', marker='v', linestyle='--', markersize=12, label=r'$16^{th}$ percentile')
+	legend = axes.legend(handles=[perc84, perc50, perc16], loc='center right', handlelength=2, fontsize=20)
+	axes.add_artist(legend)
+	data_plot = utils.medians_2d(*args, **kwargs)
+	axes.errorbar(data_plot['median_x'], data_plot['median_y'], yerr=data_plot['err_y'],
+	              marker='o', ms=8, alpha=1, linestyle='-', capsize=0)
+	axes.errorbar(data_plot['median_x'], data_plot['percent16_y'], yerr=data_plot['err_y'],
+	              marker='v', ms=8, alpha=1, linestyle='--', capsize=0)
+	axes.errorbar(data_plot['median_x'], data_plot['percent84_y'], yerr=data_plot['err_y'],
+	              marker='^', ms=8, alpha=1, linestyle='-.', capsize=0)
 
 redshift = 'z000p000'
 aperture = 7
@@ -53,14 +73,14 @@ m500 = utils.read_snap_output(redshift, apertureID=aperture, dataset='m500')
 c_l  = utils.read_snap_output(redshift, apertureID=aperture, dataset='c_l')
 figname = f'bahamas_hyd_alignment_{redshift}.png'
 
+
 fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.set_xscale("log")
 ax.set_yscale("linear")
 ax.set_xlabel(utils.datasets_names['m500'])
 ax.set_ylabel(utils.datasets_names['c_l'])
-
-ax.scatter(m500, c_l[:,1,1], marker='.', c='yellowgreen', s=3, alpha=0.3, label='load')
+median_plot(ax, m500, c_l[:,1,1], axscales = ['log', 'linear'], binning_method = 'bayesian')
 save_plot(os.path.join(utils.basepath, figname), to_slack=True, dpi=300)
 
 
