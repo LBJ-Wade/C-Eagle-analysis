db01ed90dd942a55b45434f5cb4912995d922591
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-06-18 11:58:01 +0100

    MPI and CSRM

--- rotvel_correlation/utils.py
+++ rotvel_correlation/utils.py
@@ -1,12 +1,77 @@
 import sys
 import os
-from typing import Tuple, List
+from typing import Dict, List
 import warnings
 import itertools
+import h5py
 import numpy as np
 import pandas as pd
 import scipy.stats as st
+from multiprocessing.dummy import Pool as ThreadPool
 
+basepath = '/local/scratch/altamura/analysis_results/alignment_project'
+
+def pull_halo_output(h5file, clusterID, apertureID, dataset):
+	"""
+	Function to extract a dataset from a Bahamas snapshot output.
+	:param h5file: The h5py File object to extract the data from
+	:param clusterID: The number of cluster in the order imposed by FoF
+	:param apertureID: int(0-22) The index of the spherical aperture centred on the CoP
+	:param dataset: The name of the dataset to pull
+	:return: None if the cluster does not exist in the file, the dataset as np.ndarray if it exists
+	"""
+	if f'halo_{clusterID:05d}' not in h5file:
+		warnings.warn(f"[-] Cluster {clusterID} not found in snap output.")
+		return None
+	else:
+		return h5file[f'halo_{clusterID:05d}/aperture{apertureID:02d}/{dataset}'][...]
+
+def read_snap_output(redshift: str, apertureID: int = 7, dataset: str = None) -> np.ndarray:
+	"""
+	Function to collect datasets from all clusters at given redshift and aperture.
+	The function is a wrapper around `pull_halo_output`, called within the multiprocessing/threading
+	modules. It multithreads the I/O function without the use of MPI for performance.
+	:param redshift: The redshift as a string in EAGLE format
+	:param apertureID: int(0-22) The index of the spherical aperture centred on the CoP
+	:param dataset: The name of the dataset to pull
+	:return: The merged dataset selected taken from all snapshot clusters.
+	"""
+	snapname = f'bahamas_hyd_alignment_{redshift}.hdf5'
+	h5file = h5py.File(os.path.join(basepath, snapname), 'r')
+	last_halo_id = int(h5file.keys()[:-1][-5:])
+	clusterIDs = list(range(last_halo_id))
+
+	# Make the Pool of workers
+	pool = ThreadPool(12)
+	results = pool.starmap(
+			pull_halo_output,
+			zip(
+					itertools.repeat(h5file),
+					clusterIDs,
+					itertools.repeat(apertureID),
+					itertools.repeat(dataset)
+			)
+	)
+
+	# Close the pool and wait for the work to finish
+	h5file.close()
+	pool.close()
+	pool.join()
+	results = [x for x in results if x != None]
+	results = np.asarray(results)
+	return results
+
+def output_as_dict(redshift: str, apertureID: int = 7) -> Dict[str, np.ndarray]:
+	snap_out = {}
+	for dataset in datasets_names:
+		snap_out[dataset] = read_snap_output(redshift, apertureID=apertureID, dataset=dataset)
+	return snap_out
+
+def output_as_pandas(redshift: str, apertureID: int = 7) -> pd.DataFrame:
+	snap_dict = output_as_pandas(redshift, apertureID = apertureID)
+	snap_pd = pd.DataFrame(data=snap_dict, columns=snap_dict.keys())
+	del snap_dict
+	return snap_pd
 
 def bayesian_blocks(t):
     """Bayesian Blocks Implementation
