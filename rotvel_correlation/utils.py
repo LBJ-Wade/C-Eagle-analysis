0e5df260ba843c5026bfaaadc364d0da1b2d402a
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-06-18 13:25:53 +0100

    MPI and CSRM

--- rotvel_correlation/utils.py
+++ rotvel_correlation/utils.py
@@ -74,54 +74,65 @@ def output_as_pandas(redshift: str, apertureID: int = 7) -> pd.DataFrame:
 	del snap_dict
 	return snap_pd
 
-def bayesian_blocks(t, scale: str = 'linear'):
+def bayesian_blocks(t):
     """Bayesian Blocks Implementation
+
     By Jake Vanderplas.  License: BSD
     Based on algorithm outlined in http://adsabs.harvard.edu/abs/2012arXiv1207.5578S
+
     Parameters
     ----------
     t : ndarray, length N
         data to be histogrammed
+
     Returns
     -------
     bins : ndarray
         array containing the (N+1) bin edges
+
     Notes
     -----
     This is an incomplete implementation: it may fail for some
     datasets.  Alternate fitness functions and prior forms can
     be found in the paper listed above.
     """
-
     # copy and sort the array
     t = np.sort(t)
     N = t.size
+
     # create length-(N + 1) array of cell edges
-    edges = np.concatenate([t[:1], 0.5 * (t[1:] + t[:-1]), t[-1:]])
+    edges = np.concatenate([t[:1],
+                            0.5 * (t[1:] + t[:-1]),
+                            t[-1:]])
     block_length = t[-1] - edges
+
     # arrays needed for the iteration
     nn_vec = np.ones(N)
     best = np.zeros(N, dtype=float)
     last = np.zeros(N, dtype=int)
 
+    #-----------------------------------------------------------------
+    # Start with first data cell; add one cell at each iteration
+    #-----------------------------------------------------------------
     for K in range(N):
         # Compute the width and count of the final bin for all possible
         # locations of the K^th changepoint
-        if scale == 'linear':
-            width = block_length[:K + 1] - block_length[K + 1]
-        elif scale == 'log':
-            width = np.log10(block_length[:K + 1]) - np.log10(block_length[K + 1])
-            width = 10**width
+        width = block_length[:K + 1] - block_length[K + 1]
         count_vec = np.cumsum(nn_vec[:K + 1][::-1])[::-1]
+
         # evaluate fitness function for these possibilities
         fit_vec = count_vec * (np.log(count_vec) - np.log(width))
         fit_vec -= 4  # 4 comes from the prior on the number of changepoints
         fit_vec[1:] += best[:K]
+
         # find the max of the fitness: this is the K^th changepoint
         i_max = np.argmax(fit_vec)
         last[K] = i_max
         best[K] = fit_vec[i_max]
 
+    #-----------------------------------------------------------------
+    # Recover changepoints by iteratively peeling off the last block
+    #-----------------------------------------------------------------
     change_points =  np.zeros(N, dtype=int)
     i_cp = N
     ind = N
@@ -132,6 +143,7 @@ def bayesian_blocks(t, scale: str = 'linear'):
             break
         ind = last[ind - 1]
     change_points = change_points[i_cp:]
+
     return edges[change_points]
 
 def freedman_diaconis(x: np.ndarray) -> np.ndarray:
@@ -217,10 +229,7 @@ def medians_2d(x: np.ndarray, y: np.ndarray, axscales: List[str] = None, binning
 	if axscales[0] == 'linear':
 		x_bin_stats = x_binning(x)
 	elif axscales[0] == 'log':
-		if binning_method == 'bayesian':
-			x_bin_stats = x_binning(x, scale=axscales[0])
-		else:
-			x_bin_stats = 10 ** x_binning(np.log10(x))
+		x_bin_stats = 10 ** x_binning(np.log10(x))
 		print(x_bin_stats)
 
 	median_y, edges, _ = st.binned_statistic(x, y, statistic='median', bins=x_bin_stats)
